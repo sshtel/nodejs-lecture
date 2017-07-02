d5a31ead448a7e68c8fe7ccd7a9aea9a004f3da6
Steve Song <sshtel@gmail.com>
2017-07-02 21:58:28 +0900

    03_object renamed to value and object

---
+++ lecture/03_value_and_object/object.js
@@ -0,0 +1,39 @@
+//value
+const value1 = 1;
+var   value2 = 1;
+
+
+
+var person = {
+  firstname : 'Steve',
+  lastname: 'Song',
+  hello: function() {
+    console.log('Hello, ' + this.firstname + ' ' + this.lastname);
+  }
+}
+
+person.hello();
+person.firstname = 'Seunghwa';
+console.log(person['firstname']);
+
+
+//assign empty object
+person = {};
+person.hey = 'Hey';
+person.number = 1;
+
+console.log(person.hey);
+
+
+
+//object
+const person2 = {
+  firstname : 'Subin',
+  lastname: null,
+  sayhi: function() {
+    console.log('Hello, ' + this.firstname + ' ' + this.lastname);
+  }
+}
+
+person2.lastname = 'Park'; //properties can be modified
+console.log(person2);
