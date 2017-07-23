ce7efad560696e2e694592e229729886b57aa456
Steve Song <sshtel@gmail.com>
2017-07-23 15:25:47 +0900

    add sensor_value.json example

--- lecture/07_json/app.js
+++ lecture/07_json/app.js
@@ -1,3 +1,7 @@
 var obj = require('./profile.json')
 
 console.log('my name is ' + obj.firstname + ' ' + obj.lastname);
+
+var sensor_value = require('./sensor_value.json')
+console.log(sensor_value.data[0]);
+console.log(sensor_value.data[1]);
