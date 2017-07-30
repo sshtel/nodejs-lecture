3a73641fb6b524d434789b374726545ca712fb2e
Steve Song <steve@bitfinder.co>
2017-07-30 16:22:40 +0900

    fixed json sample for reading file and parse

--- lecture/07_json/app.js
+++ lecture/07_json/app.js
@@ -5,3 +5,19 @@ console.log('my name is ' + obj.firstname + ' ' + obj.lastname);
 var sensor_value = require('./sensor_value.json')
 console.log(sensor_value.data[0]);
 console.log(sensor_value.data[1]);
+
+
+
+//read text file
+var fs = require('fs')
+fs.readFile('./profile.json', (err, data) => {
+    if(err) return;
+    if(data) {
+        var obj = JSON.parse(data);
+        console.log(obj);
+
+        var str = JSON.stringify(obj)
+        console.log(str);
+    }
+});
+
